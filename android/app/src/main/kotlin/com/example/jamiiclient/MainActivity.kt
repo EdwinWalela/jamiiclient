1991bec99ae60a16048985c870185d2c621b1f30
EdwinWalela <edwinwalela@gmail.com>
2021-06-19 19:01:03 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/jamiiclient/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jamiiclient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:cryptography/cryptography.dart';
+import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:web_socket_channel/io.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+void main() {
+  // final algorithim = Ed25519();
+  // final keyPair = await algorithim.newKeyPair();
+  // final pubKey = await keyPair.extractPublicKey();
+  // final timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+  // var _hash = "";
+
+  // final pub64 = base64Encode(pubKey.bytes);
+
+  // _hash += pub64; // Add address
+  // _hash += pub64 + pub64 + pub64 + pub64; // Add candidates
+  // _hash += timestamp; // Add timestamp
+
+  // final bytes = utf8.encode(_hash); // convert hash to bytes
+  // final digest = sha512.convert(bytes); // produce digest
+
+  // // sign hash
+  // final signature = await algorithim.sign(digest.bytes, keyPair: keyPair);
+
+  // // encode64 signature
+  // final sig64 = base64Encode(signature.bytes);
+
+  // final data = "$digest|$sig64|$pub64|$pub64.$pub64.$pub64.$pub64|$timestamp";
+  // final header = {
+  //   "source": "client",
+  //   "type": "0", // vote submission
+  //   "data": "$data",
+  // };
+
+  // IO.Socket socket = IO.io('wss://091d1f7f7171.ngrok.io');
+
+  // try {
+  //   socket.onConnect((data) => {
+  //         print("Connected to Node"),
+  //         socket.emit("vote", json.encode(header)),
+  //       });
+  // } on Exception {
+  //   print("error");
+  // }
+  // Send vote
+  // final channel = IOWebSocketChannel.connect("wss://bde039a33cea.ngrok.io",
+  //     headers: header);
+
+  // // close channel
+  // channel.sink.close();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello World"),
+        ),
+        body: Text("Hello World"),
+      ),
+    );
+  }
+}
