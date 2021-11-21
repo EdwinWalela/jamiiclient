8eaaf15d2aac8ea54ae5bae655391507a8f90728
EdwinWalela <edwinwalela@gmail.com>
2021-11-21 11:39:31 +0300

    socket connection

--- lib/src/resources/SocketProvider.dart
+++ lib/src/resources/SocketProvider.dart
@@ -1,5 +1,36 @@
+import 'package:web_socket_channel/io.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'dart:convert';
+
 class SocketProvider {
   Future<String> sendVote(String vote) async {
-    return Future(() => null);
+    final data = vote;
+    final header = {
+      "source": "client",
+      "type": "0", // vote submission
+      "data": "$data",
+    };
+
+    // Connect to node
+    IO.Socket socket = IO.io('wss://af09-197-237-160-234.ngrok.io');
+
+    try {
+      socket.onConnect((data) => {
+            print("Connected to Node"),
+            socket.emit("vote", json.encode(header)),
+          });
+    } on Exception {
+      print("error");
+    }
+    // Send vote
+    final channel = IOWebSocketChannel.connect(
+        "wss://af09-197-237-160-234.ngrok.io",
+        headers: header);
+
+    // close channel
+    channel.sink.close();
+
+    final res = await channel.stream.first;
+    return res;
   }
 }
